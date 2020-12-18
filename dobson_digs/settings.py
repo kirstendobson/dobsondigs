b06e754d3ed072b55a8e331bc5752de7d065cd5c
Kirsten Dobson <kirsten.dobson@icloud.com>
2020-12-18 18:28:12 -0500

    added gitignore

--- dobson_digs/settings.py
+++ dobson_digs/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from secrets import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'bw#6b#f(so))z9v*-)$9t0)e=7(4mr5d7qi45w9karyvi(&65-'
+SECRET_KEY = 'KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
